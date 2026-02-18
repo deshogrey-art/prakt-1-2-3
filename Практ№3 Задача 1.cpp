f46fb4d3729ee581455335d2c07adf722dbd3f30
deshogrey-art <deshogrey@mail.ru>
2026-02-18 23:27:23 +0300

    Update Практ№3 Задача 1.cpp

--- Практ№3 Задача 1.cpp
+++ Практ№3 Задача 1.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <iomanip> // Для std::setw
+#include <iomanip> // Для setw
+
+using namespace std;
 
 int main() {
     const int SIZE = 9; // Размер таблицы
 
-    std::cout << "Таблица умножения:\n\n";
+    cout << "Таблица умножения:\n\n";
 
     for (int i = 1; i <= SIZE; i++) {
         for (int j = 1; j <= SIZE; j++) {
-            std::cout << std::setw(4) << i * j; // Выравнивание столбцов
+            cout << setw(4) << i * j; // Выравнивание столбцов
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 
     return 0;
